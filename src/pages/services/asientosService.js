77167b05ca700e3f0f54e35015a384d93bc41174
Sergio Ninno <saninno@gmail.com>
2026-06-12 11:29:13 -0300

    rueda mouse y logo de cargado grilla

--- src/pages/services/asientosService.js
+++ src/pages/services/asientosService.js
@@ -1,33 +1,46 @@
 export async function cargarAsientos()
+
 {
+
     try
+
     {
-        console.time("fetch");
 
         const response = await fetch(
+
+            // "http://localhost:5062/api/contabilidad/asientos/odata/CstctbAsientos",
             "https://andromeda-backend-7eko.onrender.com/api/contabilidad/asientos/odata/CstctbAsientos",
+
             {
+
                 method:"POST",
+
                 headers: {
+
                     "Content-Type":"application/json"
+
                 },
+
                 body: JSON.stringify({})
+
             }
+
         );
 
-        console.timeEnd("fetch");
-
-        console.time("json");
-
         const data = await response.json();
-
-        console.timeEnd("json");
-
+// console.log(data);
         return data;
+
     }
+
     catch(error)
+
     {
+
         console.error(error);
+
         return [];
+
     }
+
 }
