d005ed86515a6b299183b586b5a7348e3f5c23a3
Sergio Ninno <saninno@gmail.com>
2026-07-13 19:08:01 -0300

    optimizacion de carga de grilla

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -107,7 +107,7 @@ function Login({onResult}) {
               </div>
             </div>
 
-            <span className="login-version">v5.2</span>
+            <span className="login-version">v5.3</span>
 
         </div>
     </div>
